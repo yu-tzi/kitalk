8bfdd383c0195682878cfc98238c96954b6ca1e2
Kiki <zoeluoluo05673@gmail.com>
2022-12-31 14:55:57 +0800

    fix[pipe]

--- component/userList.tsx
+++ component/userList.tsx
@@ -8,7 +8,7 @@ export const UserList = () => {
     const [userList, setUserList] = useState({ name: '' })
     useEffect(() => {
         fetch('/api/conntectToSocket').then(() => {
-            socket = io('https://kitalk.vercel.app/', { path: '/socket.io/?123=456' });
+            socket = io('https://kitalk.vercel.app/', { path: 'socket.io' });
             socket.on('connect', () => {
                 console.log('connected')
             })
