8b4db9f76eac8bab709607aef98633a25ff82d59
Kiki <zoeluoluo05673@gmail.com>
2022-12-31 15:55:00 +0800

    fix[socketIO]: 因 next prod 使用 socketIO 有問題，先移除

--- component/userList.tsx
+++ component/userList.tsx
@@ -1,25 +1,9 @@
 import { ReactNode, useContext, useEffect, useState } from 'react';
 import { UserLoginContext } from './userLoginContext';
-// @ts-ignore: Unreachable code error
-const { io } = require("socket.io-client");
-let socket
 
 export const UserList = () => {
     const [userList, setUserList] = useState({ name: '' })
     useEffect(() => {
-        fetch('/api/conntectToSocket').then(() => {
-            //socket = io('https://kitalk.vercel.app/socket.io/', { path: '' });
-            //socket = io('https://kitalk.vercel.app/socket.io/', { path: '/' });
-            //socket = io('https://kitalk.vercel.app/index/', { path: '/socket.io/' });
-            socket = io('https://kitalk.vercel.app//', { path: 'socket.io/' });
-            socket.on('connect', () => {
-                console.log('connected')
-            })
-            socket.emit("hello", userId.name);
-            socket.on("hello", (arg: any) => {
-                console.log(arg); 
-            });
-        })
     }, [])
     const userId = useContext(UserLoginContext)
     return (
